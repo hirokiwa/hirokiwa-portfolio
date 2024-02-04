39eef435948d0f85df15b9e56c1d5369749c6952
hirokiwa <hirokifsys@gmail.com>
2024-02-04 09:31:24 +0900

    create WhoIAm component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -27,10 +27,46 @@ const HomeTop = React.memo(() => {
   );
 });
 
+const WhoIAm = React.memo(() => {
+  return (
+    <div className="px-48 pt-48 pb-48">
+      <div className="flex justify-center items-center">
+        <div className="relative w-1/2 flex justify-center">
+          <div>
+            <Image
+              src={
+                "https://github.com/hirokiwa/hirokiwa-portfolio/assets/89170014/4b5f1227-028b-4727-be51-4b16675965a8"
+              }
+              alt={"ひろきのイメージ"}
+              width={0}
+              height={0}
+              sizes="30vw"
+              className="w-[20vw] rounded-lg bg-slate-100 aspect-[16464/24690]"
+            />
+            <p className="text-xs">※画像はイメージです。</p>
+          </div>
+        </div>
+        <div className="w-1/2 flex justify-center">
+          <div>
+            <h2 className="text-3xl font-bold">Who I am</h2>
+            <div className="mt-12">
+              <h3 className=" text-xl font-semibold">ひろき</h3>
+              <p className="text-sm">ソフトウェアエンジニア。</p>
+              <p className="text-sm">コード書いたりデザインしたり。</p>
+              <p className="text-sm">ブラウザで動く何かを作る人。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
+
 const Home = React.memo(() => {
   return (
     <>
       <HomeTop />
+      <WhoIAm />
     </>
   );
 });
